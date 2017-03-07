19c192f98a63cca78fad3db3bb988f31924ddaad
Leonel López <leonel.lopez@webstage.com.mx>
2017-03-07 08:49:48 -0600

    gulp install

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends("admin.template.main")
+
+@section('title')
+	Inicio de la página
+@endsection
+
+@section('content')
+	<h1>Hola desde mi panel</h1>
+	<a href="#" class="btn btn-success">Soy un boton de bootstrap</a>
+@endsection
+
---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Default') | Panel de Administración</title>
+	 <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+@include('admin.partials.nav')
+<div class="container">
+	@yield('content')
+	</div>
+<footer>
+@include('admin.partials.footer')
+</footer>
+	<script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"  ></script>
+	<script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"  ></script>
+	<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
